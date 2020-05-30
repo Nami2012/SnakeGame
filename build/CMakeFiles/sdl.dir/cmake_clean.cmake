3f152b1b82c6ac78849312ece3a4b603b958714b
nami2012 <namithasadeeshbabu@gmail.com>
2020-05-30 17:59:45 +0530

    Setup SnakeGame File Structure

---
+++ build/CMakeFiles/sdl.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sdl.dir/sdl/CApp.cpp.o"
+  "sdl"
+  "sdl.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sdl.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/snakeGame.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snakeGame.dir/snakegame/main.cpp.o"
+  "CMakeFiles/snakeGame.dir/snakegame/renderer.cpp.o"
+  "CMakeFiles/snakeGame.dir/snakegame/snake.cpp.o"
+  "snakeGame"
+  "snakeGame.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/snakeGame.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
